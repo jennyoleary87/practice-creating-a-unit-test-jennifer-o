eeebcab3211a76e8403b8ebcf5c6bccc7ea3f341
jennyo <jennifer.oleary.10@gmail.com>
2025-06-15 08:01:54 -0500

    comment test checklist

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,8 +3,10 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+//The test class mirrors the name of the class being tested (with Test at the end)
 public class MainTest {
     @Test
+    //There is at least one test for the normal (“happy path”) case
     public void testMainPositive() {
         Main person = new Main("Ashley Cole", 32);
         String realName = person.getName();
@@ -14,6 +16,8 @@ public class MainTest {
     }
 
     @Test
+    //The test method name clearly describes what is being tested
+    //The test can run independently and doesn’t rely on other tests
     public void testEmptyName() {
         Main person = new Main("", 74);
         String realName = person.getName();
@@ -24,6 +28,8 @@ public class MainTest {
 
     @Test
     public void testIsAdult() {
+        //There is at least one test for an edge case or unusual input
+        //The test uses appropriate assertions to verify behavior
         Main adult = new Main("Emily Shelton", 18);
         assertTrue(adult.isAdult());
 
@@ -32,3 +38,10 @@ public class MainTest {
     }
 
 }
+
+//The test class mirrors the name of the class being tested (with Test at the end)
+//The test method name clearly describes what is being tested
+//The test can run independently and doesn’t rely on other tests
+//There is at least one test for the normal (“happy path”) case
+//There is at least one test for an edge case or unusual input
+//The test uses appropriate assertions to verify behavior
